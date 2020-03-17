aae2475ebd33b58f1ebe2341c73a4caf2c22c85c
Yan Kuang <yk17568@bristol.ac.uk>
2020-03-17 11:32:13 +0000

    updated reduce to include all encounterIds

--- reduce_script.py
+++ reduce_script.py
@@ -1,12 +1,13 @@
 import csv
 from matplotlib import pyplot as plt
 import numpy as np
-from statistics import mean
-from datetime import datetime
 import pandas as pd
 
-data = pd.read_csv("data/anonymised_total_balances.csv")
-print(data['encounterId'].max())
-reduced_data = data.loc[data['encounterId']>=17544]
-print(reduced_data.head())
-reduced_data.to_csv('reduced_data.csv')
+balances = pd.read_csv('data/anonymised_total_balances.csv')
+targets = pd.read_csv('data/anonymised_targets.csv')
+
+targetids = set(targets.encounterId.unique())
+sharedids = targetids.intersection(set(balances.encounterId.unique()))
+
+new_balances = balances[balances['encounterId'].isin(sharedids)]
+new_balances.to_csv('new_balances.csv')
