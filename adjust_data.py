9ce1e1827536b9b2c0dbc231ca462b4c628f53c0
maxoblein <max.oblein@gmail.com>
2020-03-17 12:41:39 +0000

    function to add numerical targets to dataframe

--- adjust_data.py
+++ adjust_data.py
@@ -2,6 +2,18 @@ import csv
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
+import re
+
+
+def parseNumber(value, as_int=False):
+    try:
+        number = float(re.sub('[^.\-\d]', '', value))
+        if as_int:
+            return int(number + 0.5)
+        else:
+            return number
+    except ValueError:
+        return float('nan')  # or None if you wish
 
 
 def add_day_of_week(data):
@@ -12,6 +24,27 @@ def add_day_of_week(data):
     data['day of week'] = day_of_week
     return data
 
-balances = pd.read_csv('new_balances.csv')
+def fix_targets(targets):
+    target_strings = targets['valueString'].values
+    for i in range(len(target_strings)):
+        target_strings[i] = target_strings[i].replace("ml", "")
+        target_strings[i] = target_strings[i].replace(" to ", ",")
+        target_strings[i] = target_strings[i].replace(" ", "")
+        if ',' in target_strings[i]:
+            trange = target_strings[i].split(',')
+            for j in range(len(trange)):
+                trange[j] = parseNumber(trange[j])
+            target_strings[i] = np.array([float(trange[0]),float(trange[1])])
+        else:
+            target_strings[i] = np.array([0.0,0.0])
 
-add_day_of_week(balances)
+    targets['NumTargets'] = target_strings
+    return targets
+
+
+balances = pd.read_csv('new_balances.csv')
+targets = pd.read_csv('new_targets.csv')
+
+
+new_targets = fix_targets(targets)
+print(new_targets.head())
