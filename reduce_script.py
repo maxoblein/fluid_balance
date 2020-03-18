1f42d36c3afa5c9a98335e8ddc06c731b7f67d58
Yan Kuang <yk17568@bristol.ac.uk>
2020-03-18 13:11:48 +0000

    fixed discrepancies in patientIds

--- reduce_script.py
+++ reduce_script.py
@@ -9,16 +9,17 @@ targets = pd.read_csv('data/anonymised_targets.csv')
 balances = balances.drop(balances.columns[0],axis=1)
 targets = targets.drop(targets.columns[0],axis=1)
 
-targetids = set(targets.encounterId.unique())
-sharedids = targetids.intersection(set(balances.encounterId.unique()))
+new_balances = remove_negatives(balances, 'minutes since admission')
+new_targets = remove_negatives(targets, 'minutes since admission')
 
-new_balances = balances[balances['encounterId'].isin(sharedids)]
+targetids = set(new_targets.encounterId.unique())
+sharedids = targetids.intersection(set(new_balances.encounterId.unique()))
+
+new_balances = new_balances[new_balances['encounterId'].isin(sharedids)]
 new_balances = add_day_of_week(new_balances)
-new_balances = remove_negatives(new_balances, 'minutes since admission')
 new_balances.to_csv('new_balances.csv')
 
-new_targets = targets[targets['encounterId'].isin(sharedids)]
+new_targets = new_targets[new_targets['encounterId'].isin(sharedids)]
 new_targets = add_day_of_week(new_targets)
 new_targets = fix_targets(new_targets)
-new_targets = remove_negatives(new_targets, 'minutes since admission')
 new_targets.to_csv('new_targets.csv')
