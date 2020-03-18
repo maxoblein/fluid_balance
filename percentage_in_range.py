b922f8b683ed6c180636c6a13c10f1880fde2295
Yan Kuang <yk17568@bristol.ac.uk>
2020-03-18 13:56:28 +0000

    initial plot of patient fluid vs targets

--- percentage_in_range.py
+++ percentage_in_range.py
@@ -2,10 +2,12 @@ import csv
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
+from reduce_script import *
 
-balances = pd.read_csv('new_balances.csv')
-targets = pd.read_csv('new_targets.csv')
+balances = pd.read_csv('data/anonymised_total_balances.csv')
+targets = pd.read_csv('data/anonymised_targets.csv')
 
+new_balances, new_targets = reduce(balances, targets)
 
 def stay_data(balances,targets,id):
 
@@ -16,10 +18,30 @@ def stay_data(balances,targets,id):
 
     return stay_balances, stay_targets
 
+patientId = 17936
+	
+stay_balances, stay_targets = stay_data(new_balances,new_targets,patientId)
 
-stay_balances, stay_targets = stay_data(balances,targets,17014)
+#stay_balances.to_csv('_balances.csv')
+#stay_targets.to_csv('_targets.csv')
 
-stay_balances.to_csv('17014_balances.csv')
-stay_targets.to_csv('17014_targets.csv')
 
-fluid_stay = stay_balances.loc[stay_balances['longLabel'] == 'Net Body Balance (LOS)']
+fluid_stay = stay_balances.loc[stay_balances['longLabel'] == 'Net Body Balance (24hr)']
+fluid_cum = fluid_stay['cumTotal'].values
+fluid_min = fluid_stay['minutes since admission'].values
+target_stay = stay_targets['NumTargets'].values
+target_low = np.zeros(len(target_stay))
+target_high = np.zeros(len(target_stay))
+for i in range(len(target_stay)):
+	target_low[i] = target_stay[i][0]
+	target_high[i] = target_stay[i][1]
+target_min = stay_targets['minutes since admission'].values
+
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(fluid_min,fluid_cum)
+ax.scatter(target_min, target_low, s=10, c='r')
+ax.scatter(target_min, target_high, s=10, c='g')
+plt.show()
--- reduce_script.py
+++ reduce_script.py
@@ -4,22 +4,26 @@ import numpy as np
 import pandas as pd
 from adjust_data import *
 
-balances = pd.read_csv('data/anonymised_total_balances.csv')
-targets = pd.read_csv('data/anonymised_targets.csv')
-balances = balances.drop(balances.columns[0],axis=1)
-targets = targets.drop(targets.columns[0],axis=1)
 
-new_balances = remove_negatives(balances, 'minutes since admission')
-new_targets = remove_negatives(targets, 'minutes since admission')
+#	balances = pd.read_csv('data/anonymised_total_balances.csv')
+#	targets = pd.read_csv('data/anonymised_targets.csv')
 
-targetids = set(new_targets.encounterId.unique())
-sharedids = targetids.intersection(set(new_balances.encounterId.unique()))
+def reduce(balances, targets):
+	balances = balances.drop(balances.columns[0],axis=1)
+	targets = targets.drop(targets.columns[0],axis=1)
 
-new_balances = new_balances[new_balances['encounterId'].isin(sharedids)]
-new_balances = add_day_of_week(new_balances)
-new_balances.to_csv('new_balances.csv')
+	new_balances = remove_negatives(balances, 'minutes since admission')
+	new_targets = remove_negatives(targets, 'minutes since admission')
 
-new_targets = new_targets[new_targets['encounterId'].isin(sharedids)]
-new_targets = add_day_of_week(new_targets)
-new_targets = fix_targets(new_targets)
-new_targets.to_csv('new_targets.csv')
+	targetids = set(new_targets.encounterId.unique())
+	sharedids = targetids.intersection(set(new_balances.encounterId.unique()))
+
+	new_balances = new_balances[new_balances['encounterId'].isin(sharedids)]
+	new_balances = add_day_of_week(new_balances)
+	#new_balances.to_csv('new_balances.csv')
+
+	new_targets = new_targets[new_targets['encounterId'].isin(sharedids)]
+	new_targets = add_day_of_week(new_targets)
+	new_targets = fix_targets(new_targets)
+	#new_targets.to_csv('new_targets.csv')
+	return new_balances, new_targets
