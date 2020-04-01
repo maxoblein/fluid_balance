c23a2a11e0eb3c5b3ad9fdafff5749009f3b393d
maxoblein <max.oblein@gmail.com>
2020-04-01 15:47:13 +0100

    functionalised

--- percentage_in_range.py
+++ percentage_in_range.py
@@ -13,14 +13,14 @@ def stay_data(balances,targets,id):
     return stay_balances, stay_targets
 
 
-def main(argv):
+def patient_bal_tar_plot(id,plot=False):
     balances = pd.read_csv('data/anonymised_total_balances.csv')
     targets = pd.read_csv('data/anonymised_targets.csv')
 
     new_balances, new_targets = reduce_data(balances, targets)
 
     if len(argv) > 0:
-        patientId = int(argv[0])
+        patientId = int(id[0])
     else:
         patientId = 17771 # Default patientId, can adjust this manually if no command line input
 
@@ -30,11 +30,11 @@ def main(argv):
     stay_balances.to_csv(str(patientId) + '_balances.csv')
     stay_targets.to_csv(str(patientId) + '_targets.csv')
 
-    
+
     fluid_stay = stay_balances.loc[stay_balances['longLabel'] == 'Net Body Balance (24hr)']
     fluid_cum = fluid_stay['cumTotal'].values
     fluid_min = fluid_stay['minutes since admission'].values
-    
+
     target_stay = stay_targets['NumTargets'].values
     target_low = np.zeros(len(target_stay))
     target_high = np.zeros(len(target_stay))
@@ -42,16 +42,17 @@ def main(argv):
         target_low[i] = target_stay[i][0]
         target_high[i] = target_stay[i][1]
     target_min = stay_targets['minutes since admission'].values
-    
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.plot(fluid_min, fluid_cum)
-    ax.plot(target_min, target_low, linewidth=2, c='r')
-    ax.plot(target_min, target_high, linewidth=2, c='g')
-    plt.show()
+    if plot == True
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(fluid_min, fluid_cum)
+        ax.scatter(target_min, target_low, linewidth=2, c='r')
+        ax.scatter(target_min, target_high, linewidth=2, c='g')
+        plt.show()
+        
 
-    
+
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    patient_bal_tar_plot(sys.argv[1:],plot = True)
