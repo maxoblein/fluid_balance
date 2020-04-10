2eae2b7564a63684e8dc3c5218f2c2df9b7551e5
maxoblein <max.oblein@gmail.com>
2020-04-10 15:04:58 +0100

    ability to check all patients slowly

--- percentage_in_range.py
+++ percentage_in_range.py
@@ -18,25 +18,25 @@ def stay_data(balances, targets, id):
     return stay_balances, stay_targets
 
 
-def patient_bal_tar_plot(id, plot=False, outcsv=False):
+def patient_bal_tar_plot(id=17771, plot=False, outcsv=False):
     balances = pd.read_csv('data/anonymised_total_balances.csv')
     targets = pd.read_csv('data/anonymised_targets.csv')
 
     new_balances, new_targets = reduce_data(balances, targets)
 
-    # Check commandline
-    if len(id) > 0:
-        patientId = int(id[0])
-    else:
-        # Default patientId, can adjust this manually if no command line input
-        patientId = 17771
+    # # Check commandline
+    # if len(id) > 0:
+    #     patientId = int(id[0])
+    # else:
+    #     # Default patientId, can adjust this manually if no command line input
+    #     patientId = 17771
 
     # Extract data for one patientId
-    stay_balances, stay_targets = stay_data(new_balances, new_targets, patientId)
+    stay_balances, stay_targets = stay_data(new_balances, new_targets, id)
 
     if outcsv == True:
-        stay_balances.to_csv(str(patientId) + '_balances.csv')
-        stay_targets.to_csv(str(patientId) + '_targets.csv')
+        stay_balances.to_csv(str(id) + '_balances.csv')
+        stay_targets.to_csv(str(id) + '_targets.csv')
 
 
     # Extract fluid info
@@ -73,8 +73,8 @@ def patient_bal_tar_plot(id, plot=False, outcsv=False):
         target_av = 0.5*(compareMat[i,2] + compareMat[i,3])
         dist_from_av.append((compareMat[i,1] - target_av)**2)
     Euc_dist = np.sqrt(sum(dist_from_av))
-    print('Total Euclidean distance for ID', patientId, 'is:', Euc_dist)
-
+    print('Total Euclidean distance for ID', id, 'is:', Euc_dist)
+    return(Euc_dist)
 
     if plot == True:
         fig = plt.figure()
@@ -83,13 +83,29 @@ def patient_bal_tar_plot(id, plot=False, outcsv=False):
         ax.plot(plotMat[:,0], plotMat[:,2], c='r', label='Target lower bound')
         ax.plot(plotMat[:,0], plotMat[:,3], c='g', label='Target upper bound')
         ax.legend()
-        ax.set_title('Fluids plot for ID ' + str(patientId))
+        ax.set_title('Fluids plot for ID ' + str(id))
         ax.set_xlabel('Minutes since admission')
         ax.set_ylabel('Fluids balance [ml]')
         plt.show()
+
+def rank_patients():
+    balances = pd.read_csv('data/anonymised_total_balances.csv')
+    targets = pd.read_csv('data/anonymised_targets.csv')
+    new_balances, new_targets = reduce_data(balances, targets)
+
+    ids = np.unique(new_targets['encounterId'].values)
+    print(np.size(ids))
+    distances = np.zeros(np.size(ids))
+    for i in range(np.size(ids)):
+        print(ids[i])
+        distances[i] = patient_bal_tar_plot(id=ids[i])
+
+    print(distances)
+
 
 
 
 
 if __name__ == "__main__":
-    patient_bal_tar_plot(sys.argv[1:], plot=True, outcsv=True)
+    #patient_bal_tar_plot(sys.argv[1:], plot=True, outcsv=True)
+    rank_patients()
